744dbc5b4e8d5a34849eec8afe32566835d62af8
changraekang <crkang@covision.co.kr>
2023-09-06 17:29:37 +0900

    타입변경

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post("/post-endpoint", async (req, res) => {
   }
 
   try {
-    await client.lpush("name", name);
+    await client.lpush("name", `${name}`);
     res.json({
       message: `Hello, ${name}!`,
     });
